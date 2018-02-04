432ec3dadfdce1b7aba610d2158e5a95f92af8d3
BasilJohn <ferdenend@gmail.com>
2018-02-04 18:28:55 +0530

    Post Ad Final screen.
    Post Ad Final screen.

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,3 +2,4 @@ export * from './Footer';
 export * from './slidingmenu';
 export * from './ImageButton';
 export * from './Input';
+export * from './switch';
--- src/components/screens/postadtwo.js
+++ src/components/screens/postadtwo.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
-import { View,Text,StyleSheet, FlatList, TouchableOpacity } from 'react-native';
-import { ImageButton } from '../common';
+import { View,Text,StyleSheet,TextInput , KeyboardAvoidingView } from 'react-native';
+import { ImageButton, SwitchButton } from '../common';
 import CommonStyles from '../../styles/commonStyles';
 import {  SearchBar  } from "react-native-elements";
 
 export default class PostAd extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+           switch1Value: false,
+           switch2Value: false,
+        }
+     }
+     toggleSwitch1 = (value) => {
+        this.setState({switch1Value: value})
+        console.log('Switch 1 is: ' + value)
+     }
+     toggleSwitch2 = (value) => {
+        this.setState({switch2Value: value})
+        console.log('Switch 2 is: ' + value)
+     }
+     
     render(){
         return(
-            <View><Text>HI</Text></View>
+            <KeyboardAvoidingView>
+            <View >
+             <Text style={[CommonStyles.titleText, CommonStyles.blackColor,CommonStyles.extraBold]}>
+             Price USD
+            </Text>
+            <View style={CommonStyles.textInputField}>
+            <TextInput
+              placeholder=''
+              style={CommonStyles.textInput}
+              underlineColorAndroid='transparent'
+            />
+            </View>
+            </View>
+            <View>
+             <Text style={[CommonStyles.titleText, CommonStyles.blackColor,CommonStyles.extraBold]}>
+             Negotiable
+            </Text>
+            <View>
+             <SwitchButton/>
+            </View>
+            </View>
+            </KeyboardAvoidingView>
         )
     }
 }
