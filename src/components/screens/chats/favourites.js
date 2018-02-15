6433e0c89bc458ec0be81340777c9cabd5919872
BasilJohn <ferdenend@gmail.com>
2018-02-16 00:59:41 +0530

    Favourites initial commit
    Favourites initial commit

--- src/components/screens/chats/favourites.js
+++ src/components/screens/chats/favourites.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { View , Text } from 'react-native';
+import { View ,Text,StyleSheet,FlatList } from 'react-native';
+import ChatItem from '../chats/chatitem';
 
 export default class Favourites extends React.Component {
 
     render(){
         return(
-          <View>
-              <Text>Favourites</Text>
-          </View>   
+        //     <FlatList
+        // data={this.props.tripList}
+        // keyExtractor={(x, i) => i}
+        // renderItem={({ item }) =>
+        // <ChatItem  trip={item}/>
+        //   }>
+        //    </FlatList>
+        <View>
+            <Text>Favourites</Text>
+        </View>
         )
     }
 }
