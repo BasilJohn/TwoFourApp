33ed008e6ba9255422fba09db2ad424ca1a52997
BasilJohn <ferdenend@gmail.com>
2018-02-15 08:36:21 +0530

    New API
    New API

--- src/components/screens/home.js
+++ src/components/screens/home.js
@@ -15,10 +15,11 @@ export  default class Home extends Component {
   _keyExtractor = (item, index) => item.id;
 
   renderRowItem = (itemData) => {
-
+    // console.log('https://image.tmdb.org/t/p/w500'+itemData.item.poster_path)
     return (
       <TouchableOpacity style={styles.imageContainerStyle} onPress={this._openadDetails.bind(this)}>
-        <Image style={styles.imageStyle} source={{ uri: itemData.item.imageUrl }} resizeMode='cover' />
+        <Image style={styles.imageStyle} source={{ uri: 'https://image.tmdb.org/t/p/w500'+itemData.item.poster_path }} resizeMode='cover' />
+        {/* <Text>{itemData.item.original_title}</Text> */}
       </TouchableOpacity>
     )
   }
@@ -33,12 +34,30 @@ export  default class Home extends Component {
   componentWillMount(){
     {this.getMoviesFromApiAsync()}
   }
+
+  // async  getMoviesFromApi() {
+  //   try {
+  //     let response = await fetch(
+  //       'https://api.themoviedb.org/3/movie/popular?api_key=e248a915f47615796d83a0de440f6755&language=en-US&page=1'
+  //     );
+  //     let responseJson = await response.json();
+  //     this.setState({ moviesList: json.parse(responseJson.results) });
+  //     return responseJson.movies;
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
   getMoviesFromApiAsync = () => {
-    return fetch('http://droidtute.com/reactexample/sample_api/getMovieList.php')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({ moviesList: responseJson.movieList })
-        return responseJson.movieList;
+    return fetch('https://api.themoviedb.org/3/movie/popular?api_key=e248a915f47615796d83a0de440f6755&language=en-US&page=1')
+      .then(response =>{
+        if (response.ok) {
+          return response.json();
+        } else {
+          // handle error 
+        }
+      }).then((responseJson) => {
+        this.setState({ moviesList: responseJson.results })
+        return responseJson.results;
       })
       .catch((error) => {
         console.error(error);
