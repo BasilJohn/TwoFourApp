e33f1a4d98147fc48da84629d5e5e339f2b66862
BasilJohn <ferdenend@gmail.com>
2018-05-05 20:39:15 +0530

    Android updates
    Android updates

--- android/app/src/main/java/com/twofourapp/MainApplication.java
+++ android/app/src/main/java/com/twofourapp/MainApplication.java
@@ -3,61 +3,22 @@ package com.twofourapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.horcrux.svg.SvgPackage;
-//import com.reactnativenavigation.NavigationReactPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.horcrux.svg.SvgPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.NavigationApplication;
-
+import com.reactnative.ivpusic.imagepicker.PickerPackage; 
 import java.util.Arrays;
 import java.util.List;
 
-// public class MainApplication extends NavigationApplication {
+public class MainApplication extends NavigationApplication {
 
-//   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-//     @Override
-//     public boolean getUseDeveloperSupport() {
-//       return BuildConfig.DEBUG;
-//     }
-
-//     @Override
-//     protected List<ReactPackage> getPackages() {
-//       return Arrays.<ReactPackage>asList(
-//           new MainReactPackage(),
-//             new VectorIconsPackage(),
-//             new SvgPackage(),
-//             new NavigationReactPackage(),
-//             new LinearGradientPackage(),
-//             new PickerPackage()
-//       );
-//     }
-
-//     @Override
-//     protected String getJSMainModuleName() {
-//       return "index";
-//     }
-//   };
-
-//   @Override
-//   public ReactNativeHost getReactNativeHost() {
-//     return mReactNativeHost;
-//   }
-
-//   @Override
-//   public void onCreate() {
-//     super.onCreate();
-//     SoLoader.init(this, /* native exopackage */ false);
-//   }
-// }
-
- public class MainApplication extends NavigationApplication {
-
-     @Override
+  
      public boolean isDebug() {
          // Make sure you are using BuildConfig from your own application
          return BuildConfig.DEBUG;
@@ -67,12 +28,11 @@ import java.util.List;
          // Add additional packages you require here
          // No need to add RnnPackage and MainReactPackage
          return Arrays.<ReactPackage>asList(
-                new MainReactPackage(),
-            new VectorIconsPackage(),
-            new SvgPackage(),
-            //new NavigationReactPackage(),
-            new LinearGradientPackage(),
-            new PickerPackage()
+              new SvgPackage(),
+              new PickerPackage(),
+               new VectorIconsPackage(),
+              new LinearGradientPackage()
+            
          );
      }
 
@@ -80,20 +40,7 @@ import java.util.List;
      public List<ReactPackage> createAdditionalReactPackages() {
          return getPackages();
      }
-
-     @Override
-    public String getJSMainModuleName() {
-    return "index";
-    }
-
-  //     @Override
-  // public ReactNativeHost getReactNativeHost() {
-  //   return mReactNativeHost;
-  // }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
- }
+    
+    
+   
+}
