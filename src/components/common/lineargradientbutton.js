1ea350de195a5ed740789c95b02681e890679a68
BasilJohn <ferdenend@gmail.com>
2019-01-28 23:46:55 +0530

    Upgraded to latest version of react native
    Upgraded to latest version of react native

--- src/components/common/lineargradientbutton.js
+++ src/components/common/lineargradientbutton.js
@@ -16,7 +16,7 @@ const LinearGradientButton = props => {
         styles.LinearGradientStyle,
         {
           height: props.height,
-          width: deviceWidth - props.width,
+          width: deviceWidth,
           borderRadius: props.borderRadius
         }
       ]}
@@ -29,7 +29,7 @@ const LinearGradientButton = props => {
           styles.buttonStyle,
           {
             height: props.height,
-            width: deviceWidth - props.width,
+            width: deviceWidth,
             borderRadius: props.borderRadius
           }
         ]}
